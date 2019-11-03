8966f4427ef0014cb0f64037ccbaff2a0745752a
Liam <liamreadsspam@gmail.com>
2019-11-03 13:38:37 -0400

    Refactor/fix potion effect functions with data

--- src/main/java/com/atherys/skills/api/util/LivingUtils.java
+++ src/main/java/com/atherys/skills/api/util/LivingUtils.java
@@ -2,6 +2,7 @@ package com.atherys.skills.api.util;
 
 import org.spongepowered.api.data.DataTransactionResult;
 import org.spongepowered.api.data.key.Keys;
+import org.spongepowered.api.data.manipulator.mutable.PotionEffectData;
 import org.spongepowered.api.effect.potion.PotionEffect;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.event.cause.entity.damage.source.DamageSource;
@@ -13,27 +14,15 @@ import java.util.Optional;
 public class LivingUtils {
 
     public static DataTransactionResult applyPotionEffect(Living living, PotionEffect effect) {
-        Optional<List<PotionEffect>> potionEffects = living.get(Keys.POTION_EFFECTS);
-
-        if (potionEffects.isPresent()) {
-            List<PotionEffect> effects = potionEffects.get();
-            effects.add(effect);
-            return living.offer(Keys.POTION_EFFECTS, effects);
-        } else {
-            return living.offer(Keys.POTION_EFFECTS, Arrays.asList(effect));
-        }
+        PotionEffectData effects = living.getOrCreate(PotionEffectData.class).get();
+        effects.addElement(effect);
+        return living.offer(effects);
     }
 
     public static DataTransactionResult removePotionEffect(Living living, PotionEffect effect) {
-        Optional<List<PotionEffect>> potionEffects = living.get(Keys.POTION_EFFECTS);
-
-        if (potionEffects.isPresent()) {
-            List<PotionEffect> effects = potionEffects.get();
-            effects.remove(effect);
-            return living.offer(Keys.POTION_EFFECTS, effects);
-        } else {
-            return DataTransactionResult.failNoData();
-        }
+        PotionEffectData effects = living.getOrCreate(PotionEffectData.class).get();
+        effects.remove(effect);
+        return living.offer(effects);
     }
 
     public static boolean damageLiving(Living living, DamageSource source, double amount) {
