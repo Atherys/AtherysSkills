be22a67393605a6dd12bf11c713a4d92495f0817
Liam <liamreadsspam@gmail.com>
2019-11-27 22:30:22 -0400

    Fix modification error while iterating entity effects

--- src/main/java/com/atherys/skills/api/effect/Applyable.java
+++ src/main/java/com/atherys/skills/api/effect/Applyable.java
@@ -1,5 +1,6 @@
 package com.atherys.skills.api.effect;
 
+import com.atherys.skills.service.EffectService;
 import org.spongepowered.api.CatalogType;
 /**
  * An object which can be applied to a character and cause various temporary effects upon their game experience.
@@ -20,5 +21,10 @@ public interface Applyable extends CatalogType {
 
     boolean canRemove(long timestamp, ApplyableCarrier<?> character);
 
+    /**
+     * Will be called when {@link #canRemove(long, ApplyableCarrier)} is true.
+     *
+     * WARNING: Do not remove the effect from the carrier here. The {@link EffectService} will take care of that.
+     */
     boolean remove(long timestamp, ApplyableCarrier<?> character);
 }
