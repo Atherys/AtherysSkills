69842c4c24efd46b97d4b0ad804fc13892f86f7c
Miroslav Vasilev <miroslav.vasilev@scalefocus.com>
2019-12-04 12:26:35 +0200

    Update AtherysSkills.java
    Add version placeholder

--- src/main/java/com/atherys/skills/AtherysSkills.java
+++ src/main/java/com/atherys/skills/AtherysSkills.java
@@ -41,7 +41,7 @@ public class AtherysSkills {
 
     public static final String DESCRIPTION = "A skill plugin for the A'therys Horizons server";
 
-    public static final String VERSION = "1.0.0a";
+    public static final String VERSION = "%PLUGIN_VERSION%";
 
     private static AtherysSkills instance;
 
