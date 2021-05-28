aa742259dcfca4b4d72e85ff0bf23db37902d270
Liam <liamreadsspam@gmail.com>
2021-05-28 19:02:17 -0300

    Use intersecting entities method

--- src/main/java/com/atherys/skills/api/skill/TargetedCastable.java
+++ src/main/java/com/atherys/skills/api/skill/TargetedCastable.java
@@ -2,86 +2,49 @@ package com.atherys.skills.api.skill;
 
 import com.atherys.skills.AtherysSkills;
 import com.atherys.skills.api.exception.CastException;
-import com.flowpowered.math.imaginary.Quaterniond;
-import com.flowpowered.math.vector.Vector3d;
-import com.flowpowered.math.vector.Vector3i;
 import org.spongepowered.api.block.BlockType;
-import org.spongepowered.api.data.property.entity.EyeLocationProperty;
-import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.living.Living;
-import org.spongepowered.api.util.Tuple;
 import org.spongepowered.api.util.blockray.BlockRay;
 import org.spongepowered.api.util.blockray.BlockRayHit;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.extent.EntityUniverse;
 
-import java.util.HashSet;
 import java.util.Optional;
-import java.util.Set;
+import java.util.function.Predicate;
 
 public interface TargetedCastable extends Castable {
+    Predicate<BlockRayHit<World>> blockFilter = hit -> {
+        BlockType type = hit.getExtent().getBlockType(hit.getBlockPosition());
+        return !AtherysSkills.getInstance().getConfig().PASSABLE_BLOCKS.contains(type);
+    };
+
     @Override
     default CastResult cast(Living user, long timestamp, String... args) throws CastException {
         double range = getRange(user);
-        BlockRay<World> blockRay = BlockRay.from(user).distanceLimit(range).build();
-        Set<Vector3i> locations = new HashSet<>();
 
-        // Cast ray once so we know what blocks are visible
-        while (blockRay.hasNext()) {
-            BlockRayHit<World> blockRayHit = blockRay.next();
-            BlockType blockType = blockRayHit.getExtent().getBlockType(blockRayHit.getBlockPosition());
+        Optional<EntityUniverse.EntityHit> entityHit = user.getWorld()
+                .getIntersectingEntities(user, range, eHit -> {
+                    return eHit.getEntity() instanceof Living && user != eHit.getEntity();
+                })
+                .stream().reduce((accumulator, other) -> {
+                    return accumulator.getDistance() < other.getDistance() ? accumulator : other;
+                });
 
-            if (!AtherysSkills.getInstance().getConfig().PASSABLE_BLOCKS.contains(blockType)) {
-                break;
-            }
+        if (!entityHit.isPresent()) throw CastErrors.noTarget();
 
-            locations.add(blockRayHit.getBlockPosition());
+        Living target = (Living) entityHit.get().getEntity();
+
+        Optional<BlockRayHit<World>> end = BlockRay.from(user)
+                .select(blockFilter)
+                .distanceLimit(entityHit.get().getDistance())
+                .build()
+                .end();
+
+        if (!end.isPresent()) {
+            return cast(user, target, timestamp, args);
         }
 
-        final Vector3d rotation = user.getHeadRotation();
-        final Vector3d direction = Quaterniond.fromAxesAnglesDeg(rotation.getX(), -rotation.getY(), rotation.getZ()).getDirection();
-
-        // default to location if eyes can't be found for some reason
-        Optional<EyeLocationProperty> eyeLocationProperty = user.getProperty(EyeLocationProperty.class);
-        final Vector3d eyeLocation = eyeLocationProperty.map(EyeLocationProperty::getValue).orElse(user.getLocation().getPosition());
-
-        Entity closest = null;
-        double distance = 0;
-
-        for (Entity entity : user.getNearbyEntities(getRange(user))) {
-            if (entity.equals(user)) {
-                continue;
-            }
-
-            if (!(entity instanceof Living)) {
-                continue;
-            }
-
-            if (!entity.getBoundingBox().isPresent()) {
-                continue;
-            }
-
-            Optional<Tuple<Vector3d, Vector3d>> intersection = entity.getBoundingBox().get().intersects(eyeLocation, direction);
-            if (!intersection.isPresent()) {
-                continue;
-            }
-
-            Vector3d hitLoc = intersection.get().getFirst();
-            if (!locations.contains(hitLoc.toInt())) {
-                continue;
-            }
-
-            double newDistance = hitLoc.distanceSquared(eyeLocation);
-            if (closest == null || newDistance < distance) {
-                distance = newDistance;
-                closest = entity;
-            }
-        }
-
-        if (closest == null) {
-            throw CastErrors.noTarget();
-        }
-
-        return cast(user, (Living) closest, timestamp, args);
+        throw CastErrors.noTarget();
     }
 
     CastResult cast(Living user, Living target, long timestamp, String... args) throws CastException;
